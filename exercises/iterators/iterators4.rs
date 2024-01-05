aeb6300f49dfb2e8e767982c666fb5e1075eecf0
jmj <bitbucket@jager.id.au>
2024-01-05 08:36:24 -0800

    fixed iterators4

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -15,6 +13,8 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    //((0..num).fold((1,0), |(fib_n_m_1, fib_n_m_2),_| (fib_n_m_1+fib_n_m_2, fib_n_m_1))).0
+    (1..num+1).reduce(|a,b| a*b).or(Some(1)).unwrap()
 }
 
 #[cfg(test)]
