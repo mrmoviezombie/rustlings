e4efd321f5192c4949900b52933b758f07120a83
jmj <bitbucket@jager.id.au>
2024-01-05 09:25:37 -0800

    fixed arc1

--- exercises/smart_pointers/arc1.rs
+++ exercises/smart_pointers/arc1.rs
@@ -1,4 +1,4 @@
-// arc1.rs
+ // arc1.rs
 //
 // In this exercise, we are given a Vec of u32 called "numbers" with values
 // ranging from 0 to 99 -- [ 0, 1, 2, ..., 98, 99 ] We would like to use this
@@ -21,19 +21,17 @@
 //
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
 use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
+        let child_numbers = shared_numbers.clone();
         joinhandles.push(thread::spawn(move || {
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
             println!("Sum of offset {} is {}", offset, sum);
